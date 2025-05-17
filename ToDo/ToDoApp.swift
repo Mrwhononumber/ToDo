2f05bae127ec8a2ab6b4ab049253e3de307adbeb
Basem <basemxahmed@gmail.com>
2025-05-17 16:43:43 +0400

    Initial Commit

---
+++ ToDo/ToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoApp.swift
+//  ToDo
+//
+//  Created by Basem Elkady on 5/17/25.
+//
+
+import SwiftUI
+
+@main
+struct ToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
